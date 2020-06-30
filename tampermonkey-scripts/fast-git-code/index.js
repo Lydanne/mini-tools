e4e14aca90e43517fdf43f1f6fae9fb1d868f6f3
WumaCoder <1829913225@qq.com>
2020-06-30 15:59:58 +0800

    fastGitCode更新1.0.4

--- tampermonkey-scripts/fast-git-code/index.js
+++ tampermonkey-scripts/fast-git-code/index.js
@@ -1,33 +1,31 @@
 // ==UserScript==
 // @name         FastGitCode
 // @namespace    https://github.com/holleworldabc/browser-tools
-// @version      1.0.1
-// @description  fast show git code!
+// @version      1.0.4
+// @description  fast show git ccode!
 // @author       wm
 // @match        https://github.com/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  window.fgc.version = '1.0.1';
-  window.fgc.updateDate = '20200311';
+  const version = '1.0';
+  const updateDate = '20200311';
   //-------------------------------------------
   const fileNav = document.getElementsByClassName('file-navigation')[0];
   const btn = document.createElement("span");
   btn.className = "btn btn-sm btn-blue";
   btn.innerText = "Source Graph";
   btn.style.marginLeft = '8px';
-  btn.style.height = '32px';
   btn.onclick = function () {
     const url = window.location.href;
     const sourceGraph = 'https://sourcegraph.com/'
     const last = url.split('://')[1];
-    if(last.indexOf("\/tree\/") != -1){
-      var last2=last.replace('\/tree\/', '@');
-    }else{
-      var last2=last;
-    }
-    window.open(sourceGraph + last2);
+    window.open(sourceGraph + last);
   }
   fileNav.appendChild(btn);
+  //------------------------------------
+  console.log("load FastGitCode ok!");
+  console.log("version:" + version);
+  console.log("lastUpdateDate:" + updateDate);
 })()
